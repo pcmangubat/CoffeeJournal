63dbea80962b731aee9548c9ff76ec0a334876e0
pocholo <mr.pocholo@gmail.com>
2015-12-09 01:14:03 -0800

    adding files

---
+++ app/src/main/java/pocholo/coffeejournal/NewCoffeeLog.java
@@ -0,0 +1,13 @@
+package pocholo.coffeejournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCoffeeLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_coffee_log);
+    }
+}
